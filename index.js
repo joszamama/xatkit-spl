c8d210ff1f83953bbe14d003072afefbbc19d3d7
joszamama <joszamama@gmail.com>
2023-02-04 14:07:48 +0100

    fix: console log for database connection

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 database.on('error', (error) => {
-    console.log(error)
+    console.log('Database Connection Error: ' + error);
 })
 database.once('connected', () => {
     console.log('Database Connected');
