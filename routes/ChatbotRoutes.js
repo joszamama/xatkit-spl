0b357685d3a630b03ce6add6bb94b324239da592
joszamama <joszamama@gmail.com>
2023-02-08 12:42:10 +0100

    feat: automatic dockerization with chatbot definition

--- routes/ChatbotRoutes.js
+++ routes/ChatbotRoutes.js
@@ -363,7 +363,20 @@ router.post('/mine/:id/compile', async (req, res) => {
                             fs.mkdirSync(`./bots/${chatbot.id}`);
                         }
                         fs.copyFileSync('./bots/Dockerfile', `./bots/${chatbot.id}/Dockerfile`);
-                        fs.writeFileSync(`./bots/${chatbot.id}/${chatbot.id}.txt`, JSON.stringify(chatbotInfo));
+                        fs.readFile(`./bots/${chatbot.id}/Dockerfile`, 'utf-8', (err, data) => {
+                            if (err) throw err;
+                          
+                            // replace the contents
+                            let newData = data.replace(/CHANGE_NAME/g, chatbot.name);
+
+                            let newNewData = newData.replace(/CHANGE_DEF/g, JSON.stringify(chatbotInfo));
+                          
+                            // write the file
+                            fs.writeFile(`./bots/${chatbot.id}/Dockerfile`, newNewData, 'utf-8', (err) => {
+                              if (err) throw err;
+                              console.log('File contents updated!');
+                            });
+                          });
                         chatbot.compiled = true;
                         const data = await chatbot.save();
                         res.json({message: "Chatbot compiled successfully"});
